20216e2bc83fc76d9ba46271992f372c6aa6a706
caroprza <caro.prza@outlook.com>
2021-10-14 22:33:47 -0500

    concurrent problem count

--- src/ts/viewModels/concurrentProblems.ts
+++ src/ts/viewModels/concurrentProblems.ts
@@ -1,4 +1,5 @@
 import * as AccUtils from "../accUtils";
+import jsonFilex from "../appController";
 //imports barra de filtros
 import * as ko from "knockout";
 import { whenDocumentReady } from "ojs/ojbootstrap";
@@ -7,6 +8,8 @@ import ArrayDataProvider = require("ojs/ojarraydataprovider");
 import "ojs/ojknockout";
 import "ojs/ojselectcombobox";
 import "ojs/ojformlayout";
+import "ojs/ojchart";
+import "ojs/ojtoolbar";
 
 
 import * as Bootstrap from "ojs/ojbootstrap";
@@ -45,6 +48,10 @@ class ConcurrentProblemCountViewModel {
     datePickerWeek: ojDatePicker["datePicker"];
     timePicker: object;
 
+    readonly orientationValue = ko.observable("vertical");
+    hourCount = new Map();
+    dataProvider : ArrayDataProvider<any, any>;
+
   constructor() {
 
   }
@@ -61,6 +68,28 @@ class ConcurrentProblemCountViewModel {
     AccUtils.announce("Concurrent Problem Count page loaded.");
     document.title = "Concurrent Problem Count";
     // implement further logic if needed
+    let problemArray: Array<{hour: string, count: number, series: string}> = [];
+    for (let item in jsonFilex.jsonFile){
+      if (this.hourCount.has(jsonFilex.jsonFile[item].t1)){
+        let count = this.hourCount.get(jsonFilex.jsonFile[item].t1) + 1;
+        this.hourCount.set(jsonFilex.jsonFile[item].t1, count);
+      }
+      else {
+        this.hourCount.set(jsonFilex.jsonFile[item].t1, 1);
+      }
+    }
+
+    let i = 0;
+    this.hourCount.forEach((value: number, key: string) => {
+      problemArray.push({ hour: key, count: value, series: "Problems"});
+      i = i + 1;
+    });
+
+    let jsonCount = JSON.stringify(problemArray);
+
+    this.dataProvider = new ArrayDataProvider(JSON.parse(jsonCount), { keyAttributes: 'hour' });
+
+    document.getElementById("chart-container");
   }
 
   /**
