36e2c2343dd9a198109ee91f7ce1edc7f5eb0122
Ari Valenzuela <arivalenzuela23@gmail.com>
2021-11-11 22:56:39 -0600

    problem Frequency Filters Problems DONE
    Signed-off-by: Ari Valenzuela <arivalenzuela23@gmail.com>

--- src/ts/viewModels/problemFrequency.ts
+++ src/ts/viewModels/problemFrequency.ts
@@ -8,6 +8,7 @@ import Message = require("ojs/ojmessaging");
 import ArrayDataProvider = require("ojs/ojarraydataprovider");
 import "ojs/ojknockout";
 import "ojs/ojselectcombobox";
+import { ojSelectMany } from "ojs/ojselectcombobox";
 import "ojs/ojformlayout";
 import "ojs/ojchart";
 import "ojs/ojtoolbar";
@@ -23,6 +24,7 @@ import "ojs/ojdatetimepicker";
 import "ojs/ojlabel";
 import "ojs/ojformlayout";
 import "ojs/ojtimezonedata";
+import { ojSelect } from "ojs/ojselectcombobox";
 
 //targetfilters
 import { ojSelectMany } from "ojs/ojselectcombobox";
@@ -150,15 +152,18 @@ class ProblemFrequencyViewModel {
 
 
 
-  applyFilters = (
-    event: Event,
-    bindingContext: ko.BindingContext
+  applyProblemFilters = (
+    event: ojSelectMany.valueChanged<string,Record<string,string>>,
   ) => {
     this.problemCount = new Map();
+    this.selectedProblemsFiltersMap = new Map();
+    
     let i = 0;
+    this.selectProblemValue(event.detail.value);
     for(i;i<this.selectProblemValue().length;i++){
       this.selectedProblemsFiltersMap.set(this.selectProblemValue()[i],1);
     }
+    
 
     for (let item in jsonFilex.jsonFile){
       if (this.problemCount.has(jsonFilex.jsonFile[item].name)){
@@ -166,11 +171,16 @@ class ProblemFrequencyViewModel {
         this.problemCount.set(jsonFilex.jsonFile[item].name, count);
       }
       else {
-        if(this.selectedProblemsFiltersMap.has(jsonFilex.jsonFile[item].name || this.selectedProblemsFiltersMap.size==0)){
+        if(this.selectedProblemsFiltersMap.size==0){
           this.problemCount.set(jsonFilex.jsonFile[item].name, 1);
         } else{
-          //Do Nothing
+          if(this.selectedProblemsFiltersMap.has(jsonFilex.jsonFile[item].name)){
+            this.problemCount.set(jsonFilex.jsonFile[item].name, 1);
+          } else{
+            //Do Nothing
+          }
         }
+        
         
       }
 
@@ -188,7 +198,6 @@ class ProblemFrequencyViewModel {
     let jsonCount = JSON.stringify(problemArray);
     this.dataProvider = new ArrayDataProvider(JSON.parse(jsonCount), { keyAttributes: 'name' });
     this.dataObservableProvider(this.dataProvider);
-     
   }
   readonly selectProblemValue = ko.observableArray();
   
